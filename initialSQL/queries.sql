04c2dbe08edb70d1aacbf1a4651968676023102d
RaymondMoreau <rmore082@uottawa.ca>
2023-03-31 13:02:16 -0400

    Added final trigger and some more queries, doing indexes now.

--- initialSQL/queries.sql
+++ initialSQL/queries.sql
@@ -22,6 +22,24 @@ WHERE NOT roomID = ((SELECT roomID FROM bookings
 SELECT price, addy, roomNumber FROM hotelRoom
 WHERE capacity = x;
 
+-- grabs customerID based on their ssn input (ssnInput)
+SELECT customerID FROM customers
+WHERE ssn = ssnInput;
+-- grabs bookings of customer from their ID (custID)
+SELECT * FROM bookings
+WHERE customerID = custID;
+-- grabs past rentings of customer from their ID (custID)
+SELECT * FROM rentings
+WHERE customerID = custID;
+-- all together from above, grabs bookings/rentings based on their ssn (ssnInput)
+SELECT * FROM bookings
+WHERE customerID = (SELECT customerID FROM customers
+                                      WHERE ssn = ssnInput);
+SELECT * FROM rentings
+WHERE customerID = (SELECT customerID FROM customers
+                    WHERE ssn = ssnInput);
+
+
 
 -- trigger for the following updates to amenities
 CREATE TRIGGER invalidRoomID
@@ -66,6 +84,19 @@ SET microwave = 0 WHERE roomID = rmID;
 UPDATE amenities
 SET coffeeMaker = 0 WHERE roomID = rmID;
 
+
+-- trigger for employee update
+CREATE TRIGGER invalidSsn
+BEFORE UPDATE ON employee
+FOR EACH ROW
+BEGIN
+  DECLARE ssn_count INT;
+  SET ssn_count = (SELECT COUNT(*) FROM employee WHERE ssn = NEW.ssnNew);
+  IF (ssn_count > 0) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Desired ssn already exists in database';
+  END IF;
+END;
+
 -- employee wanting to update their ssn to a new ssn = ssnNew, inputting their old one as ssnOld
 UPDATE employee
 SET ssn = ssnNew WHERE ssn = ssnOld;
@@ -93,9 +124,16 @@ DELETE FROM hotelRoom WHERE addy = (
 DELETE FROM hotel WHERE chainName = chain_name;
 DELETE FROM hotelChain WHERE chainName = chain_name;
 
+-- deleting from bookings, where bookingID is preferably known (bookID)
+DELETE FROM bookings WHERE bookingID = bookID;
+-- little search function to find the bookingID based on start and end date entries
+-- entries in form: startD and endD
+SELECT * FROM booking
+         WHERE startDate = startD AND endDate = endD;
 
 
--- joining hotel and hotelRoom together for providing info on frontend
+
+-- JOIN hotel and hotelRoom together for providing info on frontend
 SELECT * hotel, hotelRoom FROM hotelRoom
 FULL OUTER JOIN hotel ON hotelRoom.addy=hotel.addy;
 
